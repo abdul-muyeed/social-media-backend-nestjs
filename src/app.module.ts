98b4a464a5e984826347a4c1cc4804df3480be3c
Abdul Muyeed <abdulmuyeed47@gmail.com>
2025-02-12 12:25:58 +0600

    morgan and env added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
+import { AuthModule } from './auth/auth.module';
+import { FriendModule } from './friend/friend.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    UserModule,
+    PostModule,
+    AuthModule,
+    FriendModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
